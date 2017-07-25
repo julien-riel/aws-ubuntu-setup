691abbed2c3553f87e4ffc5d9bb24c26e6d338a8
Ubuntu <ubuntu@ip-172-31-12-91.ec2.internal>
2017-07-25 23:50:34 +0000

    Change to user creation and xfce4 setup

--- setup-xfce4-rpd-and-user.sh
+++ setup-xfce4-rpd-and-user.sh
@@ -6,11 +6,14 @@ apt-get update
 
 # quietly add a user without password and then add a password and add user to sudoers
 adduser --gecos --disabled-password developper
-echo 'secret:password' | chpasswd
+echo 'developper:secret' | chpasswd
 sudo adduser developper sudo
 
 apt-get install -y xrdp
 
 echo xfce4-session > /home/developper/.xession
 
-# TODO : add startxfce4 to /etc/xrdp/startwm.sh
+sed -i 's/. \/etc\/X11\/Xsession/startxfce4/' /etc/xrdp/startwm.sh
+
+service xrdp restart
+
